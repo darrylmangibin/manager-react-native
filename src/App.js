18c11f43d36d0445dd3e27a3203dc7f11fd62067
Darryl <darrylmangibin@gmail.com>
2019-05-29 00:07:32 +0800

    boilerplate init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>App</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>App</Text>
+        </View>
+      </Provider>
     );
   };
 };
